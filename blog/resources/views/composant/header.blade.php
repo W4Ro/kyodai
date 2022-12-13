f298975435260f4214cb6fbab251a9c49a762487
W4Ro <adegbitewaris@gmail.com>
2022-12-13 21:17:00 +0100

    modif

---
+++ blog/resources/views/composant/header.blade.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="format-detection" content="telephone=no">
+<meta name="theme-color" content="#75dab4"/>
+<title>Tourog | Creative Agensy for Digital Age</title>
+<meta name="author" content="Themezinho">
+<meta name="description" content="Tourog | Creative Agensy for Digital Age">
+<meta name="keywords" content="creative, works, showcase, portfolio, highlight, projects, modern, tourog, digital, studio, css, animation, transition, freelancers">
+
+<!-- SOCIAL MEDIA META -->
+<meta property="og:description" content="Tourog | Creative Agensy for Digital Age">
+<meta property="og:image" content="http://www.themezinho.net/tourog/preview.png">
+<meta property="og:site_name" content="tourog">
+<meta property="og:title" content="tourog">
+<meta property="og:type" content="website">
+<meta property="og:url" content="http://www.themezinho.net/tourog">
+
+<!-- TWITTER META -->
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="@tourog">
+<meta name="twitter:creator" content="@tourog">
+<meta name="twitter:title" content="tourog">
+<meta name="twitter:description" content="Tourog | Creative Agensy for Digital Age">
+<meta name="twitter:image" content="http://www.themezinho.net/tourog/preview.png">
+
+<!-- FAVICON FILES -->
+<link href="ico/apple-touch-icon-144-precomposed.png" rel="apple-touch-icon" sizes="144x144">
+<link href="ico/apple-touch-icon-114-precomposed.png" rel="apple-touch-icon" sizes="114x114">
+<link href="ico/apple-touch-icon-72-precomposed.png" rel="apple-touch-icon" sizes="72x72">
+<link href="ico/apple-touch-icon-57-precomposed.png" rel="apple-touch-icon">
+<link href="ico/favicon.png" rel="shortcut icon">
+
+<!-- CSS FILES -->
+<link rel="stylesheet" href="{{asset('css/fontawesome.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/fancybox.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/hamburger.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/odometer.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/swiper.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<body>
+<div class="preloader">
+  <div class="layer"></div>
+  <!-- end layer -->
+  <div class="inner">
+    <figure> <img src="images/preloader.gif" alt="Image"> </figure>
+    <span>Site Loading</span> </div>
+  <!-- end inner --> 
+</div>
+<!-- end preloader -->
+<div class="page-transition">
+  <div class="layer"></div>
+  <!-- end layer --> 
+</div>
+<!-- end page-transition -->
+<nav class="site-navigation">
+  <div class="layer"></div>
+  <!-- end layer -->
+  <div class="inner">
+    <ul data-splitting>
+      <li><a href="index.html">HOME</a><i class="fas fa-caret-down"></i> <small>First page</small>
+      <ul>
+      	<li><a href="index.html">HOME SLIDER</a></li>
+      	<li><a href="index-video.html">HOME VIDEO</a></li>
+      	<li><a href="index-carousel.html">HOME CAROUSEL</a></li>
+      </ul>
+        </li>
+      <li><a href="studio.html">STUDIO</a> <small>All About Us</small> </li>
+      <li><a href="showcases.html">SHOWCASES</a> <small>Our all projects</small> </li>
+      <li><a href="blog.html">BLOG</a> <small>Recent posts</small> </li>
+      <li><a href="contact.html">CONTACT</a> <small>Say hello</small> </li>
+    </ul>
+  </div>
+  <!-- end inner --> 
+</nav>
---
+++ blog/resources/views/composant/script.blade.php
@@ -0,0 +1,8 @@
+<script src="{{asset('js/jquery.min.js')}}"></script> 
+<script src="{{asset('js/bootstrap.min.js')}}"></script> 
+<script src="{{asset('js/swiper.min.js')}}"></script> 
+<script src="{{asset('js/wow.min.js')}}"></script> 
+<script src="{{asset('js/splitting.min.js')}}"></script> 
+<script src="{{asset('js/odometer.min.js')}}"></script> 
+<script src="{{asset('js/fancybox.min.js')}}"></script> 
+<script src="{{asset('js/scripts.js')}}"></script>
