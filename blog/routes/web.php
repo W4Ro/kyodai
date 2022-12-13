2dc88afddb139679360c92618a2ddf8d294ba84a
W4Ro <adegbitewaris@gmail.com>
2022-12-13 20:08:39 +0100

    modif

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get("/home",[HomeController::class,"index"]);
+
+Route::get("/",[HomeController::class,"index"]);
