18e0700acc773dec488d8f966bbdbf91b23d57f1
Sylva-Egb <romaricegbewole@gmail.com>
2022-12-21 16:04:00 +0100

    update of the migrations folder

--- blog/database/migrations/2022_11_10_202043_create_event_categories_table.php
+++ blog/database/migrations/2022_11_10_202043_create_event_categories_table.php
@@ -13,17 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('event_categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('pseudo');
-            $table->string('email')->unique();
-            $table->longText('image')->default('profil-otacku.png');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
-            $table->integer('role')->default(0);
         });
     }
 
@@ -34,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('event_categories');
     }
 };
